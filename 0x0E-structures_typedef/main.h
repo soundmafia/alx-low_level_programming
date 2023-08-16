24cadf5b7d2d502106f7912e44c7c7c9c98175b6
Abdulsamad Ishola <damastohcute@gmail.com>
2023-08-15 17:00:56 -0700

    Write a function that frees dogs

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -3,6 +3,8 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 #endif
